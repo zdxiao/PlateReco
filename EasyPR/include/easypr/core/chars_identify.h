84552ef0466d357db6d1d2659fe126c46ff5fb39
betars <vincent.0812@qq.com>
2016-02-19 19:00:16 +0800

    change code

--- EasyPR/include/easypr/core/chars_identify.h
+++ EasyPR/include/easypr/core/chars_identify.h
@@ -2,6 +2,8 @@
 #define EASYPR_CORE_CHARSIDENTIFY_H_
 
 #include "easypr/util/kv.h"
+#include "mxnet/c_predict_api.h"
+
 #include <memory>
 #include <opencv2/opencv.hpp>
 
@@ -12,13 +14,30 @@ class CharsIdentify {
   static CharsIdentify* instance();
 
   std::pair<std::string, std::string> identify(cv::Mat input);
+  std::pair<std::string, std::string> identify2(cv::Mat input);
 
  private:
   CharsIdentify();
-
+  ~CharsIdentify();
   static CharsIdentify* instance_;
   cv::Ptr<cv::ml::ANN_MLP> ann_;
   std::shared_ptr<Kv> kv_;
+  //mxnet variable
+  PredictorHandle out = 0;  // alias for void *
+  PredictorHandle out2 = 0;  // alias for void *
+
+  // Parameters
+  int dev_type = 1;  // 1: cpu, 2: gpu
+  int dev_id = 0;  // arbitrary.
+  // Image size and channels
+  int width = 20;
+  int height = 20;
+  int channels = 1;
+  // Synset path for your model, you have to modify it
+  std::vector<std::string> synset;
+  std::vector<std::string> synset2;
+
+
 };
 }
 
