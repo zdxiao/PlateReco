f93689bc998c50f1a19a179ff0a40647415aecfd
betars <vincent.0812@qq.com>
2016-02-19 19:12:32 +0800

    add info

---
+++ EasyPR/CMakeFiles/easypr.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xujingping/PlateReco/EasyPR/src/core/chars_identify.cpp" "/home/xujingping/PlateReco/EasyPR/CMakeFiles/easypr.dir/src/core/chars_identify.cpp.o"
+  "/home/xujingping/PlateReco/EasyPR/src/core/chars_recognise.cpp" "/home/xujingping/PlateReco/EasyPR/CMakeFiles/easypr.dir/src/core/chars_recognise.cpp.o"
+  "/home/xujingping/PlateReco/EasyPR/src/core/chars_segment.cpp" "/home/xujingping/PlateReco/EasyPR/CMakeFiles/easypr.dir/src/core/chars_segment.cpp.o"
+  "/home/xujingping/PlateReco/EasyPR/src/core/core_func.cpp" "/home/xujingping/PlateReco/EasyPR/CMakeFiles/easypr.dir/src/core/core_func.cpp.o"
+  "/home/xujingping/PlateReco/EasyPR/src/core/feature.cpp" "/home/xujingping/PlateReco/EasyPR/CMakeFiles/easypr.dir/src/core/feature.cpp.o"
+  "/home/xujingping/PlateReco/EasyPR/src/core/plate.cpp" "/home/xujingping/PlateReco/EasyPR/CMakeFiles/easypr.dir/src/core/plate.cpp.o"
+  "/home/xujingping/PlateReco/EasyPR/src/core/plate_detect.cpp" "/home/xujingping/PlateReco/EasyPR/CMakeFiles/easypr.dir/src/core/plate_detect.cpp.o"
+  "/home/xujingping/PlateReco/EasyPR/src/core/plate_judge.cpp" "/home/xujingping/PlateReco/EasyPR/CMakeFiles/easypr.dir/src/core/plate_judge.cpp.o"
+  "/home/xujingping/PlateReco/EasyPR/src/core/plate_locate.cpp" "/home/xujingping/PlateReco/EasyPR/CMakeFiles/easypr.dir/src/core/plate_locate.cpp.o"
+  "/home/xujingping/PlateReco/EasyPR/src/core/plate_recognize.cpp" "/home/xujingping/PlateReco/EasyPR/CMakeFiles/easypr.dir/src/core/plate_recognize.cpp.o"
+  "/home/xujingping/PlateReco/EasyPR/src/core/xzdImageProcess.cpp" "/home/xujingping/PlateReco/EasyPR/CMakeFiles/easypr.dir/src/core/xzdImageProcess.cpp.o"
+  "/home/xujingping/PlateReco/EasyPR/src/preprocess/deface.cpp" "/home/xujingping/PlateReco/EasyPR/CMakeFiles/easypr.dir/src/preprocess/deface.cpp.o"
+  "/home/xujingping/PlateReco/EasyPR/src/preprocess/gdts.cpp" "/home/xujingping/PlateReco/EasyPR/CMakeFiles/easypr.dir/src/preprocess/gdts.cpp.o"
+  "/home/xujingping/PlateReco/EasyPR/src/preprocess/mc_data.cpp" "/home/xujingping/PlateReco/EasyPR/CMakeFiles/easypr.dir/src/preprocess/mc_data.cpp.o"
+  "/home/xujingping/PlateReco/EasyPR/src/train/ann_train.cpp" "/home/xujingping/PlateReco/EasyPR/CMakeFiles/easypr.dir/src/train/ann_train.cpp.o"
+  "/home/xujingping/PlateReco/EasyPR/src/train/svm_train.cpp" "/home/xujingping/PlateReco/EasyPR/CMakeFiles/easypr.dir/src/train/svm_train.cpp.o"
+  "/home/xujingping/PlateReco/EasyPR/src/train/train.cpp" "/home/xujingping/PlateReco/EasyPR/CMakeFiles/easypr.dir/src/train/train.cpp.o"
+  "/home/xujingping/PlateReco/EasyPR/src/util/kv.cpp" "/home/xujingping/PlateReco/EasyPR/CMakeFiles/easypr.dir/src/util/kv.cpp.o"
+  "/home/xujingping/PlateReco/EasyPR/src/util/program_options.cpp" "/home/xujingping/PlateReco/EasyPR/CMakeFiles/easypr.dir/src/util/program_options.cpp.o"
+  "/home/xujingping/PlateReco/EasyPR/src/util/util.cpp" "/home/xujingping/PlateReco/EasyPR/CMakeFiles/easypr.dir/src/util/util.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  "include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
