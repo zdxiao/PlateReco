d92657a05111fd1aa994165049b9f49e36482a6a
betars <vincent.0812@qq.com>
2016-02-19 22:15:08 +0800

    add files

--- EasyPR/CMakeFiles/Makefile.cmake
+++ EasyPR/CMakeFiles/Makefile.cmake
@@ -49,6 +49,6 @@ SET(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 SET(CMAKE_DEPEND_INFO_FILES
+  "CMakeFiles/demo.dir/DependInfo.cmake"
   "CMakeFiles/easypr.dir/DependInfo.cmake"
-  "CMakeFiles/plateRecoXzd.dir/DependInfo.cmake"
   )
